17223fab034e54b362fbeafd16705a727b5d0e97
Samuel Wang <dev.swang@gmail.com>
2012-10-02 03:58:37 +0800

    using google fusion table to be sample data instead of yql

--- web/controllers/home.js
+++ web/controllers/home.js
@@ -15,16 +15,19 @@ home = {
     }
 
     var yql = 'select * from geo.countries';
+    var yql_url = 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent(yql)+'&format=json';
+    var google_ft = 'http://ft2json.appspot.com/q?sql=SELECT%20*%20FROM%201m9d3Nu3M5hg1kXV5Z53HUjb7YX73MqILibHM3xg&limit=10000';
     $.ajax({
-      url: 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent(yql)+'&format=json',
+      url: google_ft,
       dataType: 'json',
       success: function(data){
-        var dataSource = data.query.results.place
+        //var dataSource = data.query.results.place; //yql
+        var dataSource = data.data;
         $('#sample-homepage').ggrid({
           dataSource: dataSource,
-          fieldSortability: ['name'],
+          fieldSortability: ['Country', 'CITY'],
           fieldFilterability: [],
-          fieldOrder: ['name', 'woeid'],
+          fieldOrder: ['Country', 'CITY'],
           fieldInvisible: ['placeTypeName'],
           style:{
             height: 400
